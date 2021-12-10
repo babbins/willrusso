870362255c6104e555705565676b5fec6daf7ba5
babbins <thealimahmoud@gmail.com>
2021-12-10 15:31:15 -0500

    remove unused imports

--- src/lib/work.ts
+++ src/lib/work.ts
@@ -1,7 +1,5 @@
 import fs from "fs";
-import matter from "gray-matter";
 import path from "path";
-import yaml from "js-yaml";
 
 export function getWorkMarkdown() {
   const fileContents = fs.readFileSync(
