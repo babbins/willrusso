64c4086b28b62f96dc9692009bf631b667efebe4
babbins <thealimahmoud@gmail.com>
2021-12-12 22:02:04 -0500

    Switch from mdx to md

--- src/lib/work.ts
+++ src/lib/work.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 export function getWorkMarkdown() {
   const fileContents = fs.readFileSync(
-    path.join(process.cwd(), "content/work.mdx"),
+    path.join(process.cwd(), "content/work.md"),
     "utf8"
   );
   return fileContents;
